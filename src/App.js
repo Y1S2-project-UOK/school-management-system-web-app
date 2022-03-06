009ffb35a70191dc1e8978230c45e0d73133b900
Ishan-Ravindu <ishanravindu27596@gmail.com>
2022-03-06 23:23:42 +0530

    install and setup tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 function App() {
   return (
-    <div >
-      <h1>school management system web app</h1>
-    </div>
+    <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
   );
 }
 
